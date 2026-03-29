0ab2f9fc5ae509b09d4e233f55f0f8c3685c9d77
Abhi <abhi.codes0@gmail.com>
2026-03-29 18:30:00 +0530

    update config

--- next.config.ts
+++ next.config.ts
@@ -4,9 +4,6 @@ const nextConfig: NextConfig = {
   output: "standalone",
   serverExternalPackages: ["@solana/web3.js", "@solana/spl-token", "bs58"],
   turbopack: {},
-  typescript: {
-    ignoreBuildErrors: true,
-  }
 };
 
 export default nextConfig;
